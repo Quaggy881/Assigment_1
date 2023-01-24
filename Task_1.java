0c8a562c993765724879f2b414823f5aa66a4cce
Quaggy881 <90851055+Quaggy881@users.noreply.github.com>
2023-01-24 23:53:33 +0800

    Add files via upload
    Task 1 
    A Rotary Club is organizing fund raiser selling take-away lunch sets. Price for an adult lunch set is $20 and price for kids set is $12. Customers can buy more than one adult set and kid set. Side orders can be added on at a separate price which includes $4 for salad and $5 for dessert.
    
    Task 2
    Write a program that asks for the names of three runners and the time, in minutes, it took each of them to finish a race. The program should display the names of the runners in the order that they finished.
    
    Task 3
    You have been asked to develop an application using a switch statement that allows theatre employee to enter a day of the week and then display the cost of a theatre ticket for the day. Incorporate switch and if..else statement to apply the following conditions in the application.
    Day Age Cost
    
    Monday - Thursday Under 10 Free 10 – 65 RM10 Over 65 Free
    
    Friday - Sunday Under 10 RM10 10 – 65 RM20 Over 65 RM10

---
+++ Task_1.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+public class Task_1 {
+
+	public static void main(String[] args) {
+		
+		//Declaration of variables
+		int adults = 0, kids = 0, dessert = 0, salad = 0,answer, i = 1;
+		double total;
+		
+		//to accept input
+		Scanner keyboard = new Scanner(System.in);
+		
+		do { try {
+			
+			String ans ;
+			
+			//Takes in the number of main orders
+			System.out.println("Enter the number of Adult Lunch sets you would like to purchase: ");
+			ans = keyboard.next();
+			adults = Integer.parseInt(ans);
+			
+			System.out.println("Enter the number of Kids Lunch sets you would like to purchase: ");
+			ans = keyboard.next();
+			kids = Integer.parseInt(ans);
+			
+			System.out.println("Would you like to buy any side dishes? Enter 1 for yes || any other integer for no");
+			answer = keyboard.nextInt();
+			
+			//Takes in the number of side orders
+			if (answer == 1) {
+				
+				System.out.println("Enter the number of Salads you would like to purchase: ");
+				ans = keyboard.next();
+				salad = Integer.parseInt(ans);
+				
+				System.out.println("Enter the number of Desserts you would like to purchase: ");
+				ans = keyboard.next();
+				dessert = Integer.parseInt(ans);
+			}	
+			else {
+					System.out.println("Thank you, proceed.");
+				}
+				
+			//Calculates & Outputs the total
+			total = (adults * 20) + (kids * 12) + (salad * 4) + (dessert * 5);
+			
+			System.out.println("Your total is: RM" + total);
+			
+			//Prompts user to go again or end program
+			System.out.println("To order more eneter 1 || To exit enter 2");
+			ans = keyboard.next();
+			i = Integer.parseInt(ans);
+			
+		} 
+		//Implementation of exception handling
+		catch(NumberFormatException e) {
+			System.out.println("Please enter a number");
+		}
+		catch(Exception e){
+			System.out.println("Please enter a number");
+		}
+			
+		} while(i==1);
+		
+		System.out.println("Have a good day :)");
+		
+		keyboard.close();
+	
+	}
+}
---
+++ Task_2.java
@@ -0,0 +1,90 @@
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+public class Task_2 {
+
+	public static void main(String[] args) {
+		
+		//declaration of variables
+		String name1, name2, name3, time1_1, time2_2, time3_3 ; 
+		double time1, time2, time3 ;
+		
+		//adding image to the dialogue box
+		ImageIcon icon1 = new ImageIcon("aa.jpg");
+		
+		//accepting user input for both names and time
+		name1 = JOptionPane.showInputDialog("Input the name of the first racer");
+		name2 = JOptionPane.showInputDialog("Input the name of the second racer");
+		name3 = JOptionPane.showInputDialog("Input the name of the third racer");
+		
+		time1_1 = JOptionPane.showInputDialog("Input the time of the first racer");
+		time2_2 = JOptionPane.showInputDialog("Input the time of the second racer");
+		time3_3 = JOptionPane.showInputDialog("Input the time of the third racer");
+		
+		time1 = Double.parseDouble(time1_1);
+		time2 = Double.parseDouble(time2_2);
+		time3 = Double.parseDouble(time3_3);
+		
+		//results when name1 is first place
+		if ((time1 > time2) && (time1 > time3)) {
+			if (time2 > time3) {
+				JOptionPane.showMessageDialog(null, "1st: " + name3 + " " + time3 + " minutes" + "\n\n2nd: " + name2 + " " + time2 + " minutes" + "\n\n3rd: " + name1 + " " + time1 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1 );
+			}
+			else if (time3 > time2) {
+				JOptionPane.showMessageDialog(null, "1st: " + name2 + " " + time2 + " minutes" + "\n\n2nd: " + name3 + " " + time3 + " minutes" + "\n\n3rd: " + name1 + " " + time1 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1 );				
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "1st: " + name2 + " & " + name3 + " are tied with the time of " + time2 + " minutes" + "\n\n2nd: " + name1 + " " + time1 + "minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1 );
+			}		
+		}
+		
+		//results when name2 is first place
+		else if ((time2 > time1) && (time2 > time3)) {
+			if (time1 > time3) {
+				JOptionPane.showMessageDialog(null, "1st: " + name3 + " " + time3 + " minutes" + "\n\n2nd: " + name1 + " " + time1 + "minutes" + "\n\n3rd: " + name2 + " " + time2 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1 );
+			}
+			else if (time3 > time1) {
+				JOptionPane.showMessageDialog(null, "1st: " + name1 + " " + time1 + " minutes" + "\n\n2nd: " + name3 +" " + time3 + " minutes" + "\n\n3rd: " + name2 + " " + time2 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1 );		
+			}
+			else {
+				JOptionPane.showMessageDialog(null,"1st: " + name1 + " & " + name3 + " are tied with the time of " + time1 + " minutes" + "\n\n2nd: " + name2 + " " + time2 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}
+		}
+		
+		//results when name3 is first place
+		else if ((time3 > time1)&& (time3 > time2)) {
+			if (time1 > time2) {
+				JOptionPane.showMessageDialog(null,"1st: " + name2 + " " + time2 + " minutes" + "\n\n2nd: " + name1 + " " + time1 + " minutes" + "\n\n3rd: " + name3 + " " + time3 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}
+			else if (time2 > time1) {
+				JOptionPane.showMessageDialog(null,"1st: " + name1 + " " + time1 + " minutes" + "\n\n2nd: " + name2 + " " + time2 + " minutes" + "\n\n3rd: " + name3 + " " + time3 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "1st: " + name1 + " & " + name2 + " are tied with the time of " + time1 + " minutes" + "\n\n2nd: " + name3 + " " + time3 + "minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}
+		}
+		
+		//results when there is a tie for second place, or all names have the same time
+		else {
+			if ((time1 < time2) && (time2 == time3)) {
+				JOptionPane.showMessageDialog(null,"1st: " + name1 + " " + time1 + " minutes" + "\n\n2nd: " + name2 + " & " + name3 + " are tied with the time of " + time2 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}
+			else if ((time2 < time1) && (time1 == time3)) {
+				JOptionPane.showMessageDialog(null, "1st: " + name2 + " " + time2 + " minutes" + "\n\n2nd: " + name1 + " & " + name3 + " are tied with the time of " + time1 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}
+			else if ((time3 < time1) && (time1 == time2)){
+				JOptionPane.showMessageDialog(null, "1st: " + name3 + " " + time3 + " minutes" + "\n\n2nd: " + name2 + " & " + name1 + " are tied with the time of " + time2 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Contestants " + name1 + ", " + name2 + " & " + name3 + " all have the same time of " + time1 + " minutes", "Rankings", JOptionPane.INFORMATION_MESSAGE, icon1);
+			}		
+		}
+		
+		
+		
+	
+				
+				
+
+	}
+
+}
